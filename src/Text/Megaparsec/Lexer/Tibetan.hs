75dfa9442fcfaa305082b0752aabda4e1a94c0b2
Vanessa McHale <tmchale@wisc.edu>
2017-01-12 16:59:43 -0600

    working packagage

--- src/Text/Megaparsec/Lexer/Tibetan.hs
+++ src/Text/Megaparsec/Lexer/Tibetan.hs
@@ -1,25 +1,33 @@
+{-# LANGUAGE FlexibleContexts #-}
+
+-- | Parser to parse Tibetan numerals
 module Text.Megaparsec.Lexer.Tibetan
-    ( commandLine
-    , parseNumber
+    ( parseNumber
     ) where
 
 import Data.Composition
 import qualified Data.Text as T
 import Text.Megaparsec
 import Text.Megaparsec.Text
+import Text.Megaparsec.Prim
 import System.Environment
+import Data.Either.Combinators
 
-commandLine :: IO ()
-commandLine = ((runParser parseNumber "") . T.pack . (!!0) <$> getArgs) >>= print
+-- | Yields a command line parser in case you want a command-line executable for use with another language
+readBo :: String -> Maybe Integer
+readBo = rightToMaybe . (runParser parseNumber "") . T.pack
 
+-- | Parse Tibetan numerals, returning a positive integer
 parseNumber :: Parser Integer
 parseNumber = do
     digits <- reverse <$> some parseNumeral
-    pure . (`div` 10) $ foldr ((*10) .* (+)) 0 digits
+    (pure . (`div` 10) $ foldr ((*10) .* (+)) 0 digits) <?> "tibetan integer"
 
+-- | Parse a single digit
 parseNumeral :: Parser Integer
 parseNumeral = foldr (<|>) (parseDigit '༠' 0) $ zipWith parseDigit "༠༡༢༣༤༥༦༧༨༩" (0:[1..9])
 
+-- | Parser a given char as a given integer
 parseDigit :: Char -> Integer -> Parser Integer
 parseDigit c i = do
     char c
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,7 @@ import Test.Hspec
 import Test.Hspec.Megaparsec
 import Text.Megaparsec.Lexer.Tibetan
 import Text.Megaparsec
+import TextShow.Data.Integral.Tibetan
 import qualified Data.Text as T
 
 main :: IO ()
@@ -13,6 +14,11 @@ main = hspec $ do
             (runParser parseNumber "") tibStr `shouldParse` 6320
         it "fails when given a character that isn't a tibetan numeral" $ do
             (runParser parseNumber "") `shouldFailOn` otherStr
+    describe "showBo" $ do
+        it "displays a positive integer using tibetan numerals" $ do
+            showBo 15 `shouldBe` Just ("༡༥")
+        it "fails on negative inputs" $ do
+            showBo (-12) `shouldBe` Nothing
 
 tibStr :: T.Text
 tibStr = "༦༣༢༠"
