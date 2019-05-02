7fbfaa15ccc9dafb583720e9e3b95b729020dc48
Vanessa McHale <vanessa.mchale@iohk.io>
2019-05-02 17:24:57 -0500

    Remove dependency on text-show

--- src/TextShow/Data/Integral/Tibetan.hs
+++ src/TextShow/Data/Integral/Tibetan.hs
@@ -3,21 +3,21 @@ module TextShow.Data.Integral.Tibetan ( builderBo
                                       , showBo) where
 
 import qualified Data.Text.Lazy as TL
-import           TextShow
+import Data.Text.Lazy.Builder (Builder, toLazyText, fromLazyText)
 
 -- | Display positive integer as a `String`
 --
 -- > λ > showBo 312
 -- > Just "༣༡༢"
 showBo :: Integer -> Maybe String
-showBo = fmap toString . builderBo
+showBo = fmap (TL.unpack.toLazyText) . builderBo
 
 -- | show a positive intgeger as text, return `Nothing` if that doesn't work
 builderBo :: (Integral a) => a -> Maybe Builder
 builderBo int
     | int >= 0  = Just . fromLazyText $ foldr (.) id
         (zipWith TL.replace
-            (map showT [0..9])
+            (fmap showT [0..9])
             ["༠","༡","༢","༣","༤","༥","༦","༧","༨","༩"])
         (showT (fromIntegral int :: Integer))
     | otherwise = Nothing
